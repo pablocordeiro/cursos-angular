38e4e7d7d5faf66e30fb27e96f07f5bd288eac33
Pablo Cordeiro <pabcordeiro@gmail.com>
2019-09-17 13:35:48 -0300

    finalizando calculadora

--- projetos-final-angular-8/projetos/calculadora/src/app/calculadora/services/calculadora.service.spec.ts
+++ projetos-final-angular-8/projetos/calculadora/src/app/calculadora/services/calculadora.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 
 import { CalculadoraService } from './calculadora.service';
 
@@ -9,4 +9,10 @@ describe('CalculadoraService', () => {
     const service: CalculadoraService = TestBed.get(CalculadoraService);
     expect(service).toBeTruthy();
   });
+  
+  it('deve garantir que 1 + 4 = 5',
+    inject([CalculadoraService], (service: CalculadoraService) => {
+      let soma = service.calcular(1,4,CalculadoraService.SOMA);
+      expect(soma).toEqual(5);
+    }))
 });
