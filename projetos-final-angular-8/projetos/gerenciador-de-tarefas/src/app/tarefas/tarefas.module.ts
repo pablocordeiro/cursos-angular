5d5a4cbd0bd9be831822d2a4df264ed1323a58dd
Pablo Cordeiro <pabcordeiro@gmail.com>
2019-09-20 14:48:39 -0300

    continuando gerenciador

--- projetos-final-angular-8/projetos/gerenciador-de-tarefas/src/app/tarefas/tarefas.module.ts
+++ projetos-final-angular-8/projetos/gerenciador-de-tarefas/src/app/tarefas/tarefas.module.ts
@@ -3,16 +3,20 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
-import { TarefaService } from './shared';
+import { TarefaService, TarefaConcluidaDirective } from './shared';
 import { ListarTarefaComponent } from './listar';
 import { CadastrarTarefaComponent } from './cadastrar';
+import { EditarTarefaComponent } from './editar';
+
 
 
 
 @NgModule({
   declarations: [
     ListarTarefaComponent,
-    CadastrarTarefaComponent
+    CadastrarTarefaComponent,
+    EditarTarefaComponent,
+    TarefaConcluidaDirective
   ],
   imports: [
     CommonModule,
